5084ef746db0984c250914f60d0985aaf7543383
nathan-K- <nathan.kienlen@gmail.com>
2016-05-29 15:03:54 +0200

    ajout tri aléatoire

--- main.c
+++ main.c
@@ -4,24 +4,34 @@
  * Teste sous GCC 4.8.4 et sous Debian
  */
 
+#define LIMIT_NUMBER 1000
+//plus grand nombre contenu dans le tableau
+#define NOMBRE_ELEMENT 10
+//nombre d elements a trier
+
 #include <stdlib.h>
-#include "stdio.h"
+#include <stdio.h>
 #include "time.h"
 
 //          DECLARATIONS
-void generationVecteurAleatoire(int* vectActuel, const int nombreElt);
+void generationVecteurAleatoire(int* vectActuel);
 
-void triSelection(int* vectActuel, const unsigned int nombreElt);
-unsigned int indicePlusPetit(int* liste, const unsigned int nombreElt, const unsigned int indiceDebut);
 
-void triInsertion(int* liste, const unsigned int nombreElt);
+void triAleatoire(int* vectActuel);
+int estTrie(int* vectActuel);
+
+void triSelection(int* vectActuel);
+unsigned int indicePlusPetit(int* liste, const unsigned int indiceDebut);
+
+void triInsertion(int* liste);
 void echangerElements(int* liste, const unsigned int indice1, const unsigned int indice2);
 
-void triBulles(int* liste, const unsigned int nombreElt);
+void triBulles(int* liste);
 
 void triRapide(int* liste, const unsigned int debut, const unsigned int fin);
 int randomInterval(const int debut, const int fin);
 
+void triComptage(int* liste);
 
 //          DEFINITIONS
 int main()
@@ -29,28 +39,33 @@ int main()
     clock_t start, end;
     double time_used;
 
-    const unsigned int nombreElements = 10;
+    int * vecteur = calloc(NOMBRE_ELEMENT, sizeof(int) );
+    generationVecteurAleatoire(vecteur);
 
-    int * vecteur = malloc(sizeof(int) * nombreElements);
-    generationVecteurAleatoire(vecteur, nombreElements);
+    printf("Vecteur de depart : \n");
+    int indice;
+    for (indice=0; indice<NOMBRE_ELEMENT; indice++)
+        printf("%i\r\n", vecteur[indice]);
+    printf("\n\nApres tri : \n");
 
     // ### TRI
     start = clock(); //Permet de chronometrer l algorithme de tri
 
-    //triSelection(vecteur, nombreElements);
-    //triInsertion(vecteur, nombreElements);
-    //triBulles(vecteur, nombreElements);
-    triRapide(vecteur, 0, nombreElements-1);
+    //triSelection(vecteur);
+    //triInsertion(vecteur);
+    //triBulles(vecteur);
+    //triRapide(vecteur, 0);
+    //triComptage(vecteur);
+    //triAleatoire(vecteur);
 
     end = clock();
     // ### TRI FAIT
 
 
     /* VERIFICATION VISUELLE  */
-    int indice;
-    for (indice=0; indice<nombreElements; indice++)
+    indice;
+    for (indice=0; indice<NOMBRE_ELEMENT; indice++)
         printf("%i\r\n", vecteur[indice]);
-
 
     time_used = ( (double) (end - start) );
     printf("Temps d execution : %f s\r\n", time_used / CLOCKS_PER_SEC);
@@ -60,23 +75,58 @@ int main()
 }
 
 
-void generationVecteurAleatoire(int* vectActuel, const int nombreElt)
+void generationVecteurAleatoire(int* vectActuel)
 {
-    //vectActuel = malloc(sizeof(int) * nombreElt);
+    //vectActuel = malloc(sizeof(int) * NOMBRE_ELEMENT);
 
     //on vide le vecteur
     unsigned int index = 0;
-    for (index; index < nombreElt; index++)
+    for (index; index < NOMBRE_ELEMENT; index++)
         vectActuel[index] = 0;
 
     srand ( (unsigned int) time (NULL) ); // pour avoir un "random" different d un execution a l autre ...
-    for (index=0; index<nombreElt; index++)
+    for (index=0; index<NOMBRE_ELEMENT; index++)
     {
-        vectActuel[index] = rand() % 10000; //On limite l amplitude des valeurs
+        vectActuel[index] = rand() % LIMIT_NUMBER; //On limite l amplitude des valeurs
     }
 }
 
 
+// ################ TRI ALEATOIRE ################
+/*
+ * Complexite temporelle : N/A
+ * Complexite memoire :    N/A
+ */
+
+int estTrie(int* vectActuel)
+{
+    unsigned int i;
+    for (i=0; i<NOMBRE_ELEMENT-1; i++)
+    {
+        if (vectActuel[i] > vectActuel[i+1])
+            return 0;
+    }
+    return 1;
+}
+
+void triAleatoire(int* vectActuel)
+{
+    unsigned int alea1;
+    unsigned int alea2;
+    unsigned long long compteurIterations = 0;
+
+    srand ( (unsigned int) time (NULL) );
+    while ( ! estTrie(vectActuel))
+    {
+        alea1 = (unsigned int) rand() % NOMBRE_ELEMENT;
+        alea2 = (unsigned int) rand() % NOMBRE_ELEMENT;
+        echangerElements(vectActuel, alea1, alea2);
+        compteurIterations++;
+    }
+    printf("%llu iterations\n", compteurIterations);
+}
+
+//  ################################
 
 // ################ TRI PAR SELECTION ################
 /*
@@ -84,20 +134,20 @@ void generationVecteurAleatoire(int* vectActuel, const int nombreElt)
  * Complexite memoire :    O(1)
  */
 
-unsigned int indicePlusPetit(int* liste, const unsigned int nombreElt, const unsigned int indiceDebut)
+unsigned int indicePlusPetit(int* liste, const unsigned int indiceDebut)
 //Retourne la premiere occurrence du plus petit element de la liste passee en parametre.
 {
-    if (nombreElt == 0 || nombreElt == 1)
+    if (NOMBRE_ELEMENT == 0 || NOMBRE_ELEMENT == 1)
         return 0;
 
-    if (indiceDebut > nombreElt)
+    if (indiceDebut > NOMBRE_ELEMENT)
         return -1; //erreur -> on fait planter le programme
 
     int ppe = liste[indiceDebut]; // plus petit element (actuel)
     unsigned int ipp = indiceDebut; // indice du ppe
 
     unsigned int indice = indiceDebut+1;
-    for (indice; indice<nombreElt; indice++)
+    for (indice; indice<NOMBRE_ELEMENT; indice++)
     {
         if (liste[indice] < ppe)
         {
@@ -110,15 +160,15 @@ unsigned int indicePlusPetit(int* liste, const unsigned int nombreElt, const uns
 }
 
 
-void triSelection(int* vectActuel, const unsigned int nombreElt)
+void triSelection(int* vectActuel)
 {
     unsigned int indice = 0;
     unsigned int ipp = 0;
     int tampon; //sert pour echanger deux valeurs
 
-    for ( indice ; indice < nombreElt; indice++) //Permet de creer virtuellement des sous tableaux
+    for ( indice ; indice < NOMBRE_ELEMENT; indice++) //Permet de creer virtuellement des sous tableaux
     {
-        ipp = indicePlusPetit(vectActuel, nombreElt, indice);
+        ipp = indicePlusPetit(vectActuel, indice);
         tampon = vectActuel[ipp];
         vectActuel[ipp] = vectActuel[indice];
         vectActuel[indice] = tampon;
@@ -142,12 +192,12 @@ void echangerElements(int* liste, const unsigned int indice1, const unsigned int
     liste[indice2] = tampon;
 }
 
-void triInsertion(int* liste, const unsigned int nombreElt)
+void triInsertion(int* liste)
 {
     unsigned int indiceTableau = 2;
     unsigned int indiceSousTableau;
 
-    for ( indiceTableau; indiceTableau < nombreElt; indiceTableau++ )
+    for ( indiceTableau; indiceTableau < NOMBRE_ELEMENT; indiceTableau++ )
     {
         for ( indiceSousTableau = indiceTableau; indiceSousTableau >= 1; indiceSousTableau-- )
         {
@@ -169,13 +219,13 @@ void triInsertion(int* liste, const unsigned int nombreElt)
  * Complexite memoire :    O(1)
  */
 
-void triBulles(int* liste, const unsigned int nombreElt)
+void triBulles(int* liste)
 
 {
     unsigned int i = 0;
     unsigned int j = 0;
 
-    for (i=nombreElt-1; i>1; i--)
+    for (i=NOMBRE_ELEMENT-1; i>1; i--)
     {
         for( j=0; j < i; j++)
         {
@@ -252,6 +302,36 @@ void triRapide(int* liste, const unsigned int debut, const unsigned int fin)
             triRapide(liste, debut, position-1);
         triRapide(liste, position+1, fin);
     }
+}
+
+//  ################################
+
+
+// ################ TRI COMPTAGE ################
+/*
+ * Complexite temporelle (moyenne) : O(n)
+ * Complexite memoire :    O(n)
+ */
+void triComptage(int* liste)
+{
+    unsigned int * tableauComptage = (unsigned int *) calloc( LIMIT_NUMBER , sizeof( unsigned int ));
+
+    unsigned int i;
+    for ( i=0; i<NOMBRE_ELEMENT; i++ )
+    {
+        tableauComptage[liste[i]]++;
+    }
+    unsigned int compteurListe = 0;
+    for ( i=0; i<LIMIT_NUMBER; i++ )
+    {
+        while( tableauComptage[i] != 0)
+        {
+            liste[compteurListe++] = i;
+            (tableauComptage[i])--;
+        }
+    }
+
+    free (tableauComptage);
 }
 
 //  ################################
